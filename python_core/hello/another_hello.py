cf7766c4bdb95e05fa773e93b6c3dca972ced10e
parashuram-patil <parasharam_patil@ymail.com>
2019-05-02 14:37:15 +0530

    Cmd to test if __name__ == '__main__'
    - ***\Python_Core>python_core\hello\another_hello.py
    
    P.S. - do not forget to set Python path as
    
    ***\Python_Core>set PYTHONPATH=%cd%

--- python_core/hello/another_hello.py
+++ python_core/hello/another_hello.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
     current_module_name = file_handling_util.get_file_name_from_path(__file__)
     hello(current_module_name)
 else:
-    hello(__name__)
+    hello("some another hello py")
